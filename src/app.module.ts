8bce21fbe5802e3130cf1a8e366fe2456776146a
VikaLubenets <lubenetcvika@gmail.com>
2024-11-06 18:54:25 +0100

    feat: add basic structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { TrackModule } from './modules/track/track.module';
+import { FavoritesModule } from './modules/favorites/favorites.module';
+import { ArtistModule } from './modules/artist/artist.module';
+import { AlbumModule } from './modules/album/album.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, TrackModule, FavoritesModule, ArtistModule, AlbumModule],
   controllers: [AppController],
   providers: [AppService],
 })
