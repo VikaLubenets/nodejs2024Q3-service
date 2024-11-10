b551118cd4ea9b123732ac7c5ba1cbc95be3fe20
VikaLubenets <lubenetcvika@gmail.com>
2024-11-10 18:17:19 +0100

    feat: add album controller, service and validation

--- src/modules/album/album.controller.ts
+++ src/modules/album/album.controller.ts
@@ -1,9 +1,49 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpException, HttpStatus, Param, ParseUUIDPipe, Post, Put } from '@nestjs/common';
+import { AlbumService } from './album.service';
+import { Album } from './type';
+import { CreateAlbumDto, UpdateAlbumDto } from './dto';
 
-@Controller('Album')
+@Controller('album')
 export class AlbumController {
+  constructor(private readonly albumService: AlbumService) {}
+
   @Get()
-  findAll(): string {
-    return 'This action returns all cats';
+  async findAll(): Promise<Album[]> {
+    return this.albumService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string): Promise<Album> {
+    const album = await this.albumService.findOne(id);
+    if (!album) {
+      throw new HttpException("Record with the provided id doesn't exist", HttpStatus.NOT_FOUND);
+    }
+    return album;
+  }
+
+  @Post()
+  async createAlbum(@Body() dto: CreateAlbumDto): Promise<Album> {
+    return this.albumService.createAlbum(dto);
+  }
+
+  @Put(':id')
+  async updateArtist(
+    @Param('id', new ParseUUIDPipe({ version: '4' })) id: string,
+    @Body() dto: UpdateAlbumDto,
+  ): Promise<Album> {
+    const updatedAlbum = await this.albumService.updateAlbum(id, dto);
+    if (!updatedAlbum) {
+      throw new HttpException("Record with the provided id doesn't exist", HttpStatus.NOT_FOUND);
+    }
+    return updatedAlbum;
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async deleteAlbum(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string): Promise<void> { 
+    const result = await this.albumService.deleteAlbum(id);
+    if (!result) {
+      throw new HttpException('Album not found', HttpStatus.NOT_FOUND);
+    }
   }
 }
--- src/modules/album/album.service.ts
+++ src/modules/album/album.service.ts
@@ -1,8 +1,51 @@
 import { Injectable } from '@nestjs/common';
+import { Album } from './type';
+import { v4 as uuidv4 } from 'uuid';
+import { CreateAlbumDto, UpdateAlbumDto } from './dto';
 
 @Injectable()
 export class AlbumService {
-  getHello(): string {
-    return 'Hello World!';
+  private storage: Album[] = [];
+
+  async findAll(): Promise<Album[]> {
+    return this.storage;
+  }
+
+  async findOne(id: string): Promise<Album | undefined> {
+    return this.storage.find(album => album.id === id);
+  }
+
+  async createAlbum(dto: CreateAlbumDto): Promise<Album> {
+    const newAlbum: Album = {
+      ...dto,
+      id: uuidv4(),
+    };
+    this.storage.push(newAlbum);
+    return newAlbum;
+  }
+
+  async updateAlbum(id: string, dto: UpdateAlbumDto): Promise<Album | null> {
+    const albumIndex = this.storage.findIndex(artist => artist.id === id);
+    if (albumIndex === -1) {
+      return null;
+    }
+    const album = this.storage[albumIndex];
+
+    const updatedAlbum: Album = {
+      ...album,
+      ...dto,
+    };
+    this.storage[albumIndex] = updatedAlbum;
+    return updatedAlbum;
+  }
+
+  async deleteAlbum(id: string): Promise<boolean> {
+    const albumIndex = this.storage.findIndex(album => album.id === id);
+    if (albumIndex === -1) {
+      return false;
+    }
+    this.storage.splice(albumIndex, 1);
+    return true;
   }
 }
+
