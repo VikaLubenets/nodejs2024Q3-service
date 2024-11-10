d39428d9d9c82a5ed3d371bcd6b9111c087f41ff
VikaLubenets <lubenetcvika@gmail.com>
2024-11-10 17:38:56 +0100

    feat: add track controller, service and validation

---
+++ src/modules/track/dto/index.ts
@@ -0,0 +1,4 @@
+import { CreateTrackDto } from "./createTrack.dto";
+import { UpdateTrackDto } from './updateTrack.dto'
+
+export { CreateTrackDto, UpdateTrackDto }
--- src/modules/track/track.controller.ts
+++ src/modules/track/track.controller.ts
@@ -1,9 +1,49 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpException, HttpStatus, Param, ParseUUIDPipe, Post, Put } from '@nestjs/common';
+import { TrackService } from './track.service';
+import { Track } from './type';
+import { CreateTrackDto, UpdateTrackDto } from './dto';
 
-@Controller('Track')
+@Controller('track')
 export class TrackController {
+  constructor(private readonly trackService: TrackService) {}
+
   @Get()
-  findAll(): string {
-    return 'This action returns all cats';
+  async findAll(): Promise<Track[]> {
+    return this.trackService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string): Promise<Track> {
+    const track = await this.trackService.findOne(id);
+    if (!track) {
+      throw new HttpException("Record with the provided id doesn't exist", HttpStatus.NOT_FOUND);
+    }
+    return track;
+  }
+
+  @Post()
+  async createTrack(@Body() dto: CreateTrackDto): Promise<Track> {
+    return this.trackService.createTrack(dto);
+  }
+
+  @Put(':id')
+  async updateTrack(
+    @Param('id', new ParseUUIDPipe({ version: '4' })) id: string,
+    @Body() dto: UpdateTrackDto,
+  ): Promise<Track> {
+    const updatedTrack = await this.trackService.updateTrack(id, dto);
+    if (!updatedTrack) {
+      throw new HttpException("Record with the provided id doesn't exist", HttpStatus.NOT_FOUND);
+    }
+    return updatedTrack;
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async deleteTrack(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string): Promise<void> { 
+    const result = await this.trackService.deleteTrack(id);
+    if (!result) {
+      throw new HttpException('Track not found', HttpStatus.NOT_FOUND);
+    }
   }
 }
